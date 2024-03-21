26671ccb793fea7e3ee327b5e509557491302f5a
JaymarkBesa <164279026+JaymarkBesa@users.noreply.github.com>
2024-03-21 09:21:16 +0800

    Add files via upload

---
+++ Group Exercise 1/Exercise2/try.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+ <body>
+  
+  <?php
+    $name1 = "Jester";
+    $name2 = "Jaymark";
+    $name3 = "Alfrancis";
+    $name4 = "Cholo";
+    $age1 = "21";
+    $age2 = "22";
+    $age3 = "23";
+    $age4 = "21";
+    $address1 = "Navotas City";
+    $address2 = "Caloocan City";
+    $address3 = "Valenzuela City";
+    $address4 = "Caloocan City";
+    $birthdate1 ="2002-12-23";
+    $birthdate2 ="2002-07-16";
+    $birthdate3 ="2002-08-12";
+    $birthdate4 ="2002-12-13"; 
+    $bloodtype1 ="AB";
+    $bloodtype2 ="N/A";
+    $bloodtype3 ="-AB";
+    $bloodtype4 ="O+";
+    $isActive1 ="True";
+
+   echo "Our group of friends consists of (4) four members, named: $name1, $age1. Located at $address1 born in $birthdate1 his blood type is $bloodtype1 and is an active student ($isActive1). <br> <br>
+
+His one friend named $name2 that is $age2. Located at $address2 born in $birthdate2 and his blood type is $bloodtype2 and is an active student ($isActive1). <br> <br> 
+
+The second one named $name3 he is $age3 aging like a fine wine. Located at $address3 born in $birthdate3 and his blood type is $bloodtype3 and is an active student ($isActive1).  <br> <br> 
+
+The last but not the least, meet our genius weirdly friend $name4, $age4 young but looks old. Located at $address4 born in $birthdate4 and his blood type is $bloodtype4 and is an active student ($isActive1).";
+
+
+
+ 
+?>
+
+</body>
+</html>
---
+++ Group Exercise 1/Exercise4/try.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <body>
+  
+  <?php
+    $name = "Jester";
+    $age = 21;
+
+   echo "My name is $name and $age years old.";
+
+?>
+
+</body>
+</html>
